19d59d854d86f3ec5092eba64ecf86f203826a61
ConnectMyCode <dhyanlinga11@gmail.com>
2026-03-25 19:16:46 +0530

    Commited on 25 March at 7:30 pm night

--- src/main/java/com/supporttriage/SupportTicketTriageApplication.java
+++ src/main/java/com/supporttriage/SupportTicketTriageApplication.java
@@ -1,5 +1,14 @@
 package com.supporttriage;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SupportTicketTriageApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(SupportTicketTriageApplication.class, args);
+    }
 }
+
+
--- src/main/java/com/supporttriage/controller/HealthController.java
+++ src/main/java/com/supporttriage/controller/HealthController.java
@@ -1,7 +1,7 @@
 package com.supporttriage.controller;
 
 	
-	import org.springframework.http.ResponseEntity;
+	import org.springframework.http.ResponseEntity; 
 	import org.springframework.web.bind.annotation.GetMapping;
 	import org.springframework.web.bind.annotation.RequestMapping;
 	import org.springframework.web.bind.annotation.RestController;
